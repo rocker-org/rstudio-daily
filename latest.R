0deb1c4b748202f2a1caa1b32056784797846e4d
Carl Boettiger <cboettig@gmail.com>
2018-11-15 09:08:20 -0800

    update scraper script

--- latest.R
+++ latest.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 
 pg <- httr::content(httr::GET("https://dailies.rstudio.com/rstudioserver/oss/debian9/x86_64/"), as = "text")
-doc <- xml2::read_xml(pg)
-deb <- xml2::xml_attr(xml2::xml_find_all(doc, "//tr[@id='row0']/td/a[@href]"), "href")
+doc <- xml2::read_html(pg)
+deb <- xml2::xml_attr(xml2::xml_find_first(doc, "//tbody/tr/td/a[@href]"), "href")
 download.file(deb, method="wget", dest="rstudio-server-daily-amd64.deb")
 
