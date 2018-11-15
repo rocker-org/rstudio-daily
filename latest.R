28e574b92d94328373cdd6e60690f244b88d0b7d
Carl Boettiger <cboettig@gmail.com>
2018-11-15 08:44:20 -0800

    Update latest.R

--- latest.R
+++ latest.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
-pg <- httr::content(httr::GET("http://www.rstudio.org/download/daily/server/ubuntu64/"), as = "text")
+pg <- httr::content(httr::GET("https://dailies.rstudio.com/rstudioserver/oss/debian9/x86_64/"), as = "text")
 doc <- xml2::read_xml(pg)
 deb <- xml2::xml_attr(xml2::xml_find_all(doc, "//tr[@id='row0']/td/a[@href]"), "href")
 download.file(deb, method="wget", dest="rstudio-server-daily-amd64.deb")
